0bf05e01d526e54aacf1290b827f98bfda0b46a1
Pierpaolo01 <pierpaolo.pascarella@gmail.com>
2023-12-26 11:47:19 +0100

    Configure Apollo GraphQL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/ui"],
+  modules: ["@nuxtjs/apollo", "@nuxt/ui", "@nuxtjs/tailwindcss"],
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: process.env.SHOPIFY_BASE_URL || "",
+        tokenStorage: "cookie",
+        httpLinkOptions: {
+          headers: {
+            "X-Shopify-Storefront-Access-Token":
+              process.env.SHOPIFY_PUBLIC_ACCESS_TOKEN || "",
+            Accept: "application/json",
+          },
+        },
+      },
+    },
+  },
 });
