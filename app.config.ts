f9feabc15fe8317a10179d58572eff9c25812b35
Pierpaolo01 <pierpaolo.pascarella@gmail.com>
2024-01-25 15:00:54 +0100

    Fix dark mode problems

--- app.config.ts
+++ app.config.ts
@@ -4,17 +4,17 @@ export default defineAppConfig({
     gray: "white",
     slideover: {
       overlay: {
-        background: "bg-black/80 dark:bg-black/80",
+        background: "opacity-50 dark:bg-black",
       },
-      background: "bg-black dark:bg-white",
+      background: "bg-white dark:bg-white",
       width: "w-screen max-w-2xl",
     },
     input: {
-      base: "border-2 border-gray-300 dark:border-gray-700 font-roboto",
+      base: "border border-gray-700 bg-white dark:border-gray-700 font-roboto ring-gray-700 ",
       color: {
         white: {
           outline:
-            "shadow-sm bg-black dark:bg-gray-900 text-gray-900 dark:text-black ring-none",
+            "shadow-sm bg-black bg-gray-900 text-gray-900 dark:text-black ring-none",
         },
       },
     },
