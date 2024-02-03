053dbebf8857491de9c699eca36fc7ee3aca3c3f
Pierpaolo01 <pierpaolo.pascarella@gmail.com>
2024-02-03 17:30:58 +0100

    return policy

---
+++ pages/policies/return.vue
@@ -0,0 +1,76 @@
+<template>
+  <UContainer class="y-padding space-y-6">
+    <div class="space-y-2">
+      <h6 class="font-bold">RETOURBELEID VAN THE FIGHTSTORE</h6>
+
+      <p>
+        Bij The Fightstore streven we naar
+        <span class="font-currency font-light">100%</span> klanttevredenheid bij
+        al onze producten. We begrijpen echter dat er soms redenen kunnen zijn
+        waarom je een product wilt retourneren of ruilen. Hieronder vind je onze
+        retourvoorwaarden.
+      </p>
+    </div>
+    <div class="space-y-2">
+      <h6 class="font-bold">RETOURTERMIJN</h6>
+      <p>
+        Je hebt <span class="font-currency">14</span> dagen vanaf de datum van
+        ontvangst om een product te retourneren.
+      </p>
+    </div>
+    <div class="space-y-2">
+      <h6 class="font-bold">VOORWAARDEN VOOR RETOURNEREN</h6>
+      <p>
+        Om in aanmerking te komen voor retournering, moet het product aan de
+        volgende voorwaarden voldoen: 
+        
+        <ul class="mt-2">
+            <li>
+                Het product moet in de originele
+                verpakking zijn.
+            </li>
+            <li>
+                Het product moet ongedragen zijn en voorzien van de
+                originele tags en hangers.
+            </li>
+        </ul>
+
+        <strong>Let op:</strong> Producten binnen de supplement of voeding categorie kunnen niet worden geretourneerd.
+      </p>
+    </div>
+    <div class="space-y-2">
+      <h6 class="font-bold">VERZENDKOSTEN</h6>
+      <p>
+        Verzendkosten dienen door de klant te worden betaald.
+      </p>
+    </div>
+    <div class="space-y-2">
+      <h6 class="font-bold">RETOURPROCES</h6>
+      <p>
+        Om een retour aan te vragen, neem contact met ons op via <a href="mailto:info@fightstore036.nl">info@fightstore036.nl</a>, <br>
+        <strong>Vermeld je bestelnummer, retour artikel en de reden voor retournering.</strong>
+      </p>
+    </div>
+  </UContainer>
+</template>
+
+
+<style scoped>
+strong {
+    font-weight: bold;
+}
+
+ul {
+    padding-left: 20px;
+    list-style: disc !important;
+}
+
+ul li {
+    margin-bottom: 0.5em;
+}
+
+a {
+    color: #3182ce;
+    text-decoration: underline;
+}
+</style>
