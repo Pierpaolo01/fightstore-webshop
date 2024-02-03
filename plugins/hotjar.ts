b242bb66b914dc4b8db0bd004c3c1f2308ee5dd1
Pierpaolo01 <pierpaolo.pascarella@gmail.com>
2024-02-03 18:48:50 +0100

    Revert hotjar change

--- plugins/hotjar.ts
+++
@@ -1,9 +0,0 @@
-import VueHotjar from "vue-hotjar-next";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueHotjar, {
-    id: 3852225,
-    isProduction: false,
-    snippetVersion: 6,
-  });
-});
