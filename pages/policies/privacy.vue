abd3d63b96d3d9d81b64dbe1c1e331334933887c
Pierpaolo01 <pierpaolo.pascarella@gmail.com>
2024-02-10 10:49:33 +0100

    Mouseflow on every page

--- pages/policies/privacy.vue
+++ pages/policies/privacy.vue
@@ -31,6 +31,15 @@ useHead({
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
     },
+    {
+      children: `window._mfq = window._mfq || [];
+        (function() {
+          var mf = document.createElement("script");
+          mf.type = "text/javascript"; mf.defer = true;
+          mf.src = "//cdn.mouseflow.com/projects/7d391ec4-5e29-4558-90e2-a987891e7c8c.js";
+          document.getElementsByTagName("head")[0].appendChild(mf);
+        })()`,
+    },
   ],
 });
 </script>
